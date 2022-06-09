99fe76705f8b2fc32760ffbc05ee3803b08c88b3
Pooja <100369673+Poojashree03@users.noreply.github.com>
2022-06-09 18:29:11 +0000

    problem#06.python

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -3,14 +3,31 @@
 largest = None
 smallest = None
 
-while True:
-    num = input("Enter a number? ")
+#while True:
+    #num = input("Enter a number? ")
 
-    if num == "done":
-        break
+    #if num == "done":
+        #break
 
     # ...
 
-    print(num)
+    #print(num)
 
-print("Maximum", largest)
+#print("Maximum", largest)
+
+score = input("Enter Score: ") 
+ s =  float(score) 
+ x = 'Error' 
+ if s >= 0.9: 
+         x = 'A' 
+ elif s >=0.8: 
+         x='B' 
+ elif s >=0.7: 
+         x='C' 
+ elif s >= 0.6: 
+         x='D' 
+ elif s < .6: 
+         x ='F' 
+ else: 
+         x ="Out of Range" 
+ print (x)
