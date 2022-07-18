1b305b8f4896869f4697e19e45e81dfaa0d10aed
Pooja <100369673+Poojashree03@users.noreply.github.com>
2022-07-18 03:55:57 +0000

    problem#14.python

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,25 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+import urllib.request, urllib.parse, urlllib.error
+import xml.etreee.ElementTree as ET
+
+url=input ('Enter url:')
+print('Retrieving',url)
+
+total=0
+couunt=0
+
+uh=urllib.request.urlopen(url)
+data=uh.read()
+print('Retrieveed',len(data),'characters')
+
+tree=ET.fromstring(data)
+lst=tree.findall('comments/comment')
+
+for item in lst:
+  count=count + 1
+  t=item.find ('count').text
+  total=total + float(t
+
+ print('Count:', count)
+ print('Sum:', total)                     
