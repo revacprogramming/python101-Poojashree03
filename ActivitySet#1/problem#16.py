29b4d543527e10644ec469d123e8cfdb138f5be4
Pooja <100369673+Poojashree03@users.noreply.github.com>
2022-07-20 10:47:46 +0000

    problem#16.py

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,2 +1,9 @@
 # Databases
 # https://www.py4e.com/lessons/database
+from turtle import *
+colors = ['orange', 'red', 'pink', 'yellow', 'blue', 'green']
+for x in range(360):
+pencolor(colors[x % 6])
+width(x / 5 + 1)
+forward(x)
+left(20)
