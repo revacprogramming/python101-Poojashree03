52e7064ca76e3d51be401fec18f31c71d4394db1
Pooja <100369673+Poojashree03@users.noreply.github.com>
2022-07-24 03:55:16 +0000

    pyth4on pyth5on pyth6on pyth7on pyth8on pyth9on

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,26 +1,25 @@
-
-
-def get_cs():
-    """get string input"""
-
-
-def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
-
-def lot_to_cs(lot):
-    """convert list of strings to connected string"""
-
-
-def main():
-    cs=get_cs()
-
-    lot=cs_to_lot(cs)  # convert connect string to list of tuples
-    print(lot)
-
-    cs=lot_to_cs(lot)  # convert list of strings to connect string
-    print(cs)
-
-
-if __name__ == '__main__':
-    main()
+Program 4
+def get_cs(): 
+     return(input()) 
+    
+ def cs_to_lot(cs): 
+     j=[] 
+     ll=cs.split(';') 
+     for i in ll: 
+        j.append(tuple(i.split('='))) 
+     return j 
+ def lot_to_cs(lot): 
+     return (";".join([str(a+"="+b) for a,b in lot])) 
+  
+ def main(): 
+     cs=get_cs() 
+  
+     lot=cs_to_lot(cs)  # convert connect string to list of tuples 
+     print(lot) 
+  
+     cs=lot_to_cs(lot)  # convert list of strings to connect string 
+     print(cs) 
+  
+  
+ if __name__ == '__main__': 
+     main()
--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,26 +1,26 @@
-
-
-def get_cs():
-    """get string input"""
-
-
-def cs_to_dict(cs):
-    """convert connect string to a dictionary"""
-
-
-def dict_to_cs(d):
-    """convert a dictionary to connect string"""
-
-
-def main():
-    cs = get_cs()
-
-    d = cs_to_dict(cs) # convert connect string to a dictionary
-    print(d)
-
-    cs = dict_to_cs(d)
-    print(cs)
-
-
-if __name__ == '__main__':
-    main()
+Program 5 
+def get_cs(): 
+     return input() 
+  
+ def cs_to_dict(cs): 
+     d={} 
+     for i in cs.split(';'): 
+       s=i.split('=') 
+       d[s[0]]=s[1] 
+     return d 
+  
+ def dict_to_cs(d): 
+   return (";".join([str(i+"="+d[i]) for i in d])) 
+    
+ def main(): 
+     cs = get_cs() 
+  
+     d = cs_to_dict(cs) # convert connect string to a dictionary 
+     print(d) 
+  
+     cs = dict_to_cs(d) 
+     print(cs) 
+  
+  
+ if __name__ == '__main__': 
+     main()
