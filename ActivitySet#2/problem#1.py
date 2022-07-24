01ed520ddbe56ca313df5bbdf146c9c2e1576be4
Pooja <100369673+Poojashree03@users.noreply.github.com>
2022-07-24 03:50:51 +0000

    pyth1on pyth2on pyth3on

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,13 @@
-
-
-def add(a, b):
-    return  # ...
-
-
-def main():
-    a = input  # ...
-    b = input  # ...
-
-    c = add(a, b)
-    print  # ...
+Program 1
+def add(a, b): 
+     return a+b 
+  
+  
+ def main(): 
+     a = int(input('Enter 1st Number?')) 
+     b = int(input('Enter 2st Number?')) 
+  
+     c = add(a, b) 
+     print(f'Sum of {a} and {b} is {c}') 
+  
+ main()
--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,18 +1,20 @@
-
-def add(a, b):
-    pass  # ...
-
-
-def output(a, b, sum):
-    pass  # ...
-
-
-def main():
-    a, b = input_two_numbers()
-    sum = add(a, b)
-
-    output(a, b, sum)
-
-
-if __name__ == '__main__':
-    main()
+Program 2
+def add(a, b): 
+     return a+b 
+  
+ def output(a, b, sum): 
+     print(f'{a}+{b} is {sum}') 
+  
+ def input_two_numbers(): 
+     n1,n2=input('input?').split() 
+     return int(n1),int(n2) 
+    
+ def main(): 
+     a, b = input_two_numbers() 
+     sum = add(a, b) 
+  
+     output(a, b, sum) 
+  
+  
+ if __name__ == '__main__': 
+     main()
