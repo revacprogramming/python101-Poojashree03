1748a839a3f473e0378ddabfa601a3859e80f3a4
Pooja <100369673+Poojashree03@users.noreply.github.com>
2022-07-24 03:28:36 +0000

    pyth6on

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,35 +1,20 @@
-#Loops & Iterators
-
-largest= None
-smallest= None
-
-#while True:
-  #num = input("Enter a number? ")
-
-    #if num == "done":
-        #break
-
-    # ...
-
-    #print(num)
-
-#print("Maximum", largest)
-
-score = input("Enter Score: ") 
- s =  float(score) 
-
- x = 'Error' 
- if s >= 0.9: 
-         x = 'A' 
- elif s >=0.8: 
-         x='B' 
- elif s >=0.7: 
-         x='C' 
- elif s >= 0.6: 
-         x='D' 
- elif s < .6: 
-         x ='F' 
- else: 
-         x ="Out of Range" 
-
- print (x)
+Program 6 # Loops & Iterators 
+  
+ largest = None 
+ smallest = None 
+  
+ while True: 
+     num = input("Enter a number? ") 
+     if num == "done": 
+         break 
+     try: 
+         n = int(num) 
+         if largest is None or n > largest: 
+             largest = n 
+         if smallest is None or n < smallest: 
+             smallest = n 
+     except: 
+             print('Invalid input') 
+  
+ print('Maximum is',largest) 
+ print('Minimum is',smallest)
